e37d2f842938fbd4ccb3b8cf65d6a5c5d9668b89
Siddhartha Shekhar Mishra <81522695+siddhartha-web@users.noreply.github.com>
2021-07-28 21:19:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us6.api.mailchimp.com/3.0/lists/640d4921ce";
+
+    const options = {
+      method: "POST",
+      auth: "siddhartha209:1b6161ba019653f3d8a91508d1f257cc-us6"
+    }
+
+  const request = https.request(url,options,function(response){
+
+    if(response.statusCode===200)
+    {
+      res.sendFile(__dirname + "/success.html");
+    }else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+          response.on("data",function(data){
+            console.log(JSON.parse(data));
+          })
+    })
+
+      request.write(jsonData);
+      request.end();
+
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running on port 3000");
+});
+
+// API KEY
+// 1b6161ba019653f3d8a91508d1f257cc-us6
+
+// LIST ID
+// 640d4921ce
